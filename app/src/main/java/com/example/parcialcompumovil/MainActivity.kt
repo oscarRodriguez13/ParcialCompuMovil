f963013401e88edba3697539ec6f95339789f03f
oscar <oscarrodrigomez22@gmail.com>
2024-03-14 09:46:30 -0500

    primero

--- app/src/main/java/com/example/parcialcompumovil/MainActivity.kt
+++ app/src/main/java/com/example/parcialcompumovil/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.example.parcialcompumovil
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnJugar = findViewById<Button>(R.id.jugar)
+        btnJugar.setOnClickListener {
+            val intent = Intent(this, JugarActivity::class.java)
+            startActivity(intent)
+        }
+
+        val btnCulturizate = findViewById<Button>(R.id.culturizate)
+        btnCulturizate.setOnClickListener {
+            val intent = Intent(this, CulturizateActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
